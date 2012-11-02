79f2ec95c02cc3dec71a9ecbaed4b53e93cb66d7
andreaskipf <andreas.kipf@gmail.com>
2012-11-02 12:51:05 +0100

    Initial commit

---
+++ wahlinfo/src/de/tum/wahlinfo/ConnectionHelper.java
@@ -0,0 +1,47 @@
+package de.tum.wahlinfo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ConnectionHelper {
+
+	private String url, user, password;
+	private static ConnectionHelper instance;
+
+	private ConnectionHelper() {
+		try {
+			ResourceBundle bundle = ResourceBundle.getBundle("wahlinfo");
+			Class.forName(bundle.getString("jdbc.driver"));
+			url = bundle.getString("jdbc.url");
+			user = bundle.getString("jdbc.user");
+			password = bundle.getString("jdbc.password");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		if (instance == null) {
+			instance = new ConnectionHelper();
+		}
+		try {
+			return DriverManager.getConnection(instance.url, instance.user,
+					instance.password);
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+
+	public static void close(Connection connection) {
+		try {
+			if (connection != null) {
+				connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
