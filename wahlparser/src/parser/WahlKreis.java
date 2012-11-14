1df77b5512976f7c6559a5233761d36087880ad6
alex <alexandervanrenen@gmail.com>
2012-11-14 15:20:05 +0100

    removed uneede properties of wahlkreis

--- wahlparser/src/parser/WahlKreis.java
+++ wahlparser/src/parser/WahlKreis.java
@@ -17,9 +17,5 @@ public class WahlKreis {
 		this.parteien = parteien;
 	}
 	int wahlkreisId;
-	int wahlberechtigte;
-	int wahlberechtigte2005;
-	int waehler;
-	int waehler2005;
 	HashMap<Integer, Partei> parteien;
 }
