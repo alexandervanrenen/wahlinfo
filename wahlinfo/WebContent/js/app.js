69b6521c84b74bdf9ba6dbf7f6cdf5de2a7b4c4c
andreaskipf <andreas.kipf@gmail.com>
2013-01-11 15:11:43 +0100

    Finally integrated the google chart in angular

--- wahlinfo/WebContent/js/app.js
+++ wahlinfo/WebContent/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('wahlinfo', ['wahlinfoServices']).
+angular.module('wahlinfo', ['wahlinfoServices', 'wahlinfoDirectives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.
     	when('/bundestag', {templateUrl: 'partials/bundestag.html', controller: BundestagCtrl}).
@@ -8,47 +8,3 @@ var app = angular.module('wahlinfo', ['wahlinfoServices']).
     	when('/vorjahresvergleich', {templateUrl: 'partials/vorjahresvergleich.html', controller: VorjahresvergleichCtrl}).
     	otherwise({redirectTo: '/bundestag'});
   }]);
-
-//app.directive('chart', function() {
-//    return function($scope, $elm, $attr, Sitzverteilung) { // TODO inject Sitzverteilung
-//    	
-//        // Create the data table.
-//        var data = new google.visualization.DataTable();
-//        data.addColumn('string', 'Partei');
-//        data.addColumn('number', 'Anzahl der Sitze');
-////        data.addRows([
-////          ['Mushrooms', 3],
-////          ['Onions', 1],
-////          ['Olives', 1],
-////          ['Zucchini', 1],
-////          ['Pepperoni', 2]
-////        ]);
-//        
-//        
-//
-//        // Set chart options
-//        var options = {'title':'Sitzverteilung im Bundestag',
-//                       'width':400,
-//                       'height':300};
-//
-//        // Instantiate and draw our chart, passing in some options.
-//        var chart = new google.visualization.PieChart($elm[0]);
-//        chart.draw(data, options);
-//        
-////        $scope.$watch($attr.chart, function(value) {
-////        	alert('watch');
-////	            // Create the data table.
-////	            var data = new google.visualization.DataTable();
-////	            data.addColumn('string', 'Partei');
-////	            data.addColumn('number', 'Anzahl der Sitze');
-////	            data.addRows(value);
-////	            
-////	            // Set chart options
-////	            var options = {'title':'Sitzverteilung im Bundestag',
-////	                           'width':400,
-////	                           'height':300};
-////	            
-////	            chart.draw(data, options);
-////    		});        
-//    }
-//});
--- wahlinfo/WebContent/js/directives.js
+++ wahlinfo/WebContent/js/directives.js
@@ -2,10 +2,36 @@
 
 /* Directives */
 
-
-//angular.module('myApp.directives', []).
-//  directive('appVersion', ['version', function(version) {
-//    return function(scope, elm, attrs) {
-//      elm.text(version);
-//    };
-//  }]);
+angular.module('wahlinfoDirectives', []).
+	directive('chart', function() {
+		return {
+			restrict: 'A',
+			link: function(scope, elm, attrs) {
+				scope.$watch('sitzverteilung', function(array) {
+					// Create the data table.
+			        var data = new google.visualization.DataTable();
+			        data.addColumn('string', 'Partei');
+			        data.addColumn('number', 'Anzahl der Sitze');
+			        
+			        // Add rows
+			        var farben = new Array();
+			        for (var i in array) {
+			        	data.addRow([array[i].name, parseInt(array[i].sitze)]);
+			        	farben[i] = array[i].farbe;
+			        }
+			        
+			        // Set chart options
+			        var options = {title: 'Sitzverteilung im Bundestag',
+			                       width: 800,
+			                       height: 400,
+			                       // TODO set colors dynamically
+			                       colors: ['green', 'black', 'blue', 'purple', 'yellow', 'red']
+			        };
+		
+			        // Instantiate and draw our chart, passing in some options.
+			        var chart = new google.visualization.PieChart(elm[0]);
+			        chart.draw(data, options);
+	            }, true);
+			}
+		}
+	});
